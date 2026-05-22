cff5b9d2ebf4e067be54953ece572087fd903ea3
Vinoth Kumar <vinoth.akithon@gmail.com>
2026-05-22 20:42:12 +0530

    Updating astro configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-  site: process.env.URL || 'https://duinzengineering.com',
-  base: process.env.BASE_URL || '/',
+  site: process.env.URL || "https://vinoth-akithon.github.io",
+  base: process.env.BASE_URL || "/3d-structures/",
 });
